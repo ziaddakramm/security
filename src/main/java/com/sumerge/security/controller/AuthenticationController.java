aeb1b627740a09fa44c3699cb1ffa61e096aedcc
ziaddakramm <106281067+ziaddakramm@users.noreply.github.com>
2024-02-14 21:22:45 +0200

    change controller prefix variable name in app.properties and controller

--- src/main/java/com/sumerge/security/controller/AuthenticationController.java
+++ src/main/java/com/sumerge/security/controller/AuthenticationController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
 @RestController
-@RequestMapping("${api-prefix}")
+@RequestMapping("${controller-prefix}")
 @RequiredArgsConstructor
 public class AuthenticationController {
 
